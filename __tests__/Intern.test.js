4373e6dc98691388b4aecf9981f3df572ab8d781
David Daly <dalyd14@gmail.com>
2021-01-07 11:43:52 -0500

    complteted intern get functions

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -8,3 +8,13 @@ test('create an intern object', () => {
     expect(intern.id).toBe(1)
     expect(intern.school).toBe('Clemson University')
 })
+
+test('get values returned from intern class', () => {
+    const employee = new Intern('Dave', 'dalyd14@gmail.com', 1, 'Clemson University')
+
+    expect(employee.getName()).toBe('Dave')
+    expect(employee.getEmail()).toBe('dalyd14@gmail.com')
+    expect(employee.getId()).toBe(1)
+    expect(employee.getSchool()).toBe('Clemson University')
+    expect(employee.getRole()).toBe('Intern')
+})
