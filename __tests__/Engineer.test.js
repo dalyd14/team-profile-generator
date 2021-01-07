66ea36c49d9cbd32347f57622ea8fd7af879d16c
David Daly <dalyd14@gmail.com>
2021-01-07 12:04:03 -0500

    finalizedtests for engineers

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -8,3 +8,13 @@ test('creates engineer object', () => {
     expect(engineer.id).toBe(1)
     expect(engineer.github).toBe('dalyd14')
 })
+
+test('get values returned from engineer class', () => {
+    const engineer = new Engineer('Dave', 'dalyd14@gmail.com', 1, 'dalyd14')
+
+    expect(engineer.getName()).toBe('Dave')
+    expect(engineer.getEmail()).toBe('dalyd14@gmail.com')
+    expect(engineer.getId()).toBe(1)
+    expect(engineer.getGithub()).toBe('dalyd14')
+    expect(engineer.getRole()).toBe('Engineer')
+})
