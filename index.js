7214645eb1b4fdeb347127b80dbe119562bdf196
David Daly <dalyd14@gmail.com>
2021-01-07 14:05:26 -0500

    code to add members of the team is done

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const inquirer = require('inquirer')
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
 
 const employeeRepository = []
 
-const getManager = function() {
+const enterEmployee = () => {
     return new Promise( (resolve) => {
         inquirer.prompt([
             {
                 type: 'input',
-                name: 'managerName',
-                message: "Please enter the team manager's name:",
+                name: 'employeeName',
+                message: "Please enter the employee's name:",
                 validate: function (input) {
                     if(input === '') {
                         return "Please enter the manager's name."
@@ -19,8 +22,8 @@ const getManager = function() {
             },
             {
                 type: 'input',
-                name: 'managerEmail',
-                message: "Please enter the team manager's email:",
+                name: 'employeeEmail',
+                message: "Please enter the employee's email:",
                 validate: function (input) {
                     if(input === '' || !input.includes('@')) {
                         return "Please enter a valid email."
@@ -31,8 +34,8 @@ const getManager = function() {
             },
             {
                 type: 'number',
-                name: 'managerId',
-                message: "Please enter the team manager's employee ID:",
+                name: 'employeeId',
+                message: "Please enter the employee's ID:",
                 validate: function (input) {
                     if(isNaN(input)) {
                         return "Please enter an employee ID as a number."
@@ -40,7 +43,15 @@ const getManager = function() {
                         return true
                     }
                 }
-            },
+            }
+        ]).then( answers => {
+            resolve(answers)
+        })
+    })
+}
+const enterManager = () => {
+    return new Promise( (resolve) => {
+        inquirer.prompt(
             {
                 type: 'number',
                 name: 'managerOffice',
@@ -53,23 +64,101 @@ const getManager = function() {
                     }
                 }
             }
-        ]).then( answers => resolve(answers))
+        ).then( answers => resolve(answers))
     })
-} 
+}
+const enterEngineer = () => {
+    return new Promise( (resolve) => {
+        inquirer.prompt(
+            {
+                type: 'input',
+                name: 'engineerGithub',
+                message: "Please enter the engineer's GitHub username:",
+                validate: function (input) {
+                    if(input === '') {
+                        return "Enter the engineer's GitHub username."
+                    } else {
+                        return true
+                    }
+                }
+            }
+        ).then( answers => resolve(answers))
+    })
+}
+const enterIntern = () => {
+    return new Promise( (resolve) => {
+        inquirer.prompt(
+            {
+                type: 'input',
+                name: 'internSchool',
+                message: "Please enter the intern's school name:",
+                validate: function (input) {
+                    if(input === '') {
+                        return "Enter the intern's school name."
+                    } else {
+                        return true
+                    }
+                }
+            }
+        ).then( answers => resolve(answers))
+    })
+}
 
-const enterAnotherMember = function() {
+const newManager = () => {
+    console.log('Enter information about the Manager:')
+    enterEmployee().then( empVal => {
+        enterManager().then( manVal => {
+            const manager = new Manager(empVal.employeeName, empVal.employeeEmail, empVal.employeeId, manVal.managerOffice)
+            employeeRepository.push(manager)
+            enterAnotherMember().then( answer => nextMember(answer))
+        })
+    })
+}
+const newEngineer = () => {
+    console.log('Enter information about the Engineer:')
+    enterEmployee().then( empVal => {
+        enterEngineer().then( engVal => {
+            const engineer = new Engineer(empVal.employeeName, empVal.employeeEmail, empVal.employeeId, engVal.engineerGithub)
+            employeeRepository.push(engineer)
+            enterAnotherMember().then( answer => nextMember(answer))
+        })
+    })
+}
+const newIntern = () => {
+    console.log('Enter information about the Intern:')
+    enterEmployee().then( empVal => {
+        enterIntern().then( intVal => {
+            const intern = new Intern(empVal.employeeName, empVal.employeeEmail, empVal.employeeId, intVal.internSchool)
+            employeeRepository.push(intern)
+            enterAnotherMember().then( answer => nextMember(answer))
+        })
+    })
+}
+
+const enterAnotherMember = () => {
     return new Promise( resolve => {
         inquirer.prompt(
             {
-                type: 'confirm',
+                type: 'list',
                 name: 'anotherMember',
-                message: "Would you like to add a team member?",
-                default: false
+                message: "Pick the team member you would like to add.",
+                choices: ['Engineer', 'Intern', 'I am done adding members']
             }
         ).then( answers => resolve(answers.anotherMember))
     })
 }
+const nextMember = (answer) => {
+    switch (answer) {
+        case 'Engineer':
+            newEngineer()
+            break;
+        case 'Intern':
+            newIntern()
+            break;
+        case 'I am done adding members':
+        default:
+            break;
+    }
+}
 
-enterAnotherMember().then( val => {
-    console.log(val)
-})
+newManager()
